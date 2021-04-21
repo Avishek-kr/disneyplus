c996f3a69c403512888c69cec93e9cfb64e593a3
Avishek-kr <ak251997@gmail.com>
2021-04-21 20:31:03 +0530

    background image

---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import React from 'react'
+
+const Login = (props) => {
+    return (
+        <BgImage />
+    
+    )
+}
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+export default Login
+ 
