5516ab663b40341f19a4f779536b6bac30923309
Avishek-kr <ak251997@gmail.com>
2021-04-22 20:15:44 +0530

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
-import "./App.css";
+// import "./App.css";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
 
